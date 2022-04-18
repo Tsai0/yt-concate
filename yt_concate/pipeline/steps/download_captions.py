c5a37efe788e58dce789258d84e0ee9899371995
Leo <leo2004411@gmail.com>
2022-04-19 00:46:18 +0800

    Finished downloading videos

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -10,24 +10,26 @@ from .step import StepException
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
-        for url in data:
-            print("download caption for", url)
+        for yt in data:
+            print("download caption for", yt.id)
 
-            if utils.caption_file_exists(url):
+            if utils.caption_file_exists(yt):
                 print('found existing caption file')
                 continue
             try:
-                source = YouTube(url)
+                source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
                 en_caption_convert_to_srt = (en_caption.generate_srt_captions())
 
             except (KeyError, AttributeError):
-                print("An Error for :", url)
+                print("An Error for :", yt.url)
                 continue
 
-            text_file = open(utils.get_caption_path(url), "w", encoding='utf-8')
+            text_file = open(yt.get_caption_path(), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
 
         end = time.time()
         print('took', end - start, 'seconds')
+
+        return data
